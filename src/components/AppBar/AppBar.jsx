9fe63f0d445b4d926fb597c7b480867eddc51bea
DENYS BUKHTAROV <denis180984@gmail.com>
2023-03-15 08:36:52 +0200

    AppBar

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,4 +1,4 @@
 import React from "react";
 import Navigatin from './Navigation/Navigation';
 import UserMenu from './UserMenu/UserMenu';
-import { Box, Flex, useColorModeValue}
+import { Box, Flex, useColorModeValue} from '@chakra-ui/react'
