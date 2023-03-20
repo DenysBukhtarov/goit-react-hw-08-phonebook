9369ccc9ff720f56a0832700eadede6ebf6faa21
DENYS BUKHTAROV <denis180984@gmail.com>
2023-03-20 12:39:32 +0200

    RegisterForm

---
+++ src/components/RegisterForm/RegisterForm.js
@@ -0,0 +1,39 @@
+import { useDispatch } from "react-redux";
+import { register } from "redux/auth/operations";
+import {Form, Label, Input, Button } from './RegisterForm.styled';
+
+
+export const RegisterFrom = () => {
+    const dispatch = useDispatch();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const from = e.currentTarget;
+        dispatch (
+            register({
+                name: from.elements.name.value,
+                email: from.elements.email.value,
+                password: from.elements.password.value,
+            })
+        );
+        from.reset();
+    };
+
+    return (
+        <Form onSubmit={handleSubmit} autoComplete="off">
+            <Label>
+                UserName
+                <Input type="text" name="name" />
+            </Label>
+            <Label>
+               Email
+                <Input type="email" name="email" />
+            </Label>
+            <Label>
+                Password
+                <Input type="password" name="password" />
+            </Label>
+            <Button type="submit">Register</Button>
+        </Form>
+    );
+};
