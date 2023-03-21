131a1c42785ec0296b2cf8fa9eebc2d11ad9dfae
DENYS BUKHTAROV <denis180984@gmail.com>
2023-03-21 13:35:28 +0200

    pages

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,97 +1,63 @@
-import { useEffect, useState } from "react";
+import { Formik } from 'formik';
+import * as yup from 'yup';
 import {
-    useAddContactsMutation,
-    useGetContactQuery
-} from '../../redux/ContactsApi';
-import { toast } from 'react-toastify';
-import { Stack, Button } from "@chakra-ui/react";
+    FormContact,
+    Label, 
+    Input,
+    Button,
+    ErrorText,
+} from './ContactForm.styled';
 
-function ContactForm() {
-    const [addContacts, { isLoading, isSuccess, error }] =
-        useAddContactsMutation();
-    const { data } = useGetContactQuery();
-    const [name, setName] = useState('');
-    const [number, setNumber] = useState('');
-    const [nameForToast, setnameForToast] = useState('');
+const nameValidate = 
+"^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$";
+const phoneValidate = RegExp(/^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{1,3}\\)[ \\-]*)|([0-9]{1,4})[ \\-]*)*?[0-9]{1,4}?[ \\-]*[0-9]{1,9}?$/);
 
-    useEffect(() => {
-        isSuccess &&
-            toast.success(`${nameForToast} added to contact book`, {
-                autoClose: 800,
-            });
-        error && toast.error('oops something went wrong');
-    }, [error, isSuccess, nameForToast]);
+const schema = yup.object().shape({
+    name: yup
+    .string()
+    .required()
+    .matches(
+        nameValidate,
+        "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+    ),
 
+    phone: yup
+    .string()
+    .required()
+    .matches(
+        phoneValidate,
+    'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    ),
+});
 
-    const handleInputChange = ({ currentTarget: { name, value } }) => {
-        switch (name) {
-            case "name":
-                setName(value);
-                break;
-            case "number":
-                setNumber(value);
-                break;
-            default:
-                return;
-        }
-    };
+const initialValues = {
+    name: '',
+    phone: '',
+};
 
-    const onFormSubmit = e => {
-
-        e.preventDefault();
-        setnameForToast(name);
-
-        data.every(item => item.name.toLowerCase() !== name.toLowerCase())
-            ? addContacts({
-                name: name,
-                phone: number,
-            })
-            : toast.error(`${name} is already in contacts!!!`);
-
-        console.log(data);
-
-        setName('');
-        setNumber('');
-    };
-
+export const ContactFrom = ({ handleSubmit }) => {
     return (
-        <form autoComplete="off" onSubmit={onFormSubmit}>
-            <Stack w="400px" mx="auto" my="6" spacing={4}>
-                <p>Name</p>
-
-                <input
-                    onChange={handleInputChange}
-                    value={name}
-                    type="text"
-                    name="name"
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. 
-                           For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    required
-                    placeholder="Jacob Mercer"
-                />
-                <p>Number</p>
-                <input
-                    onChange={handleInputChange}
-                    value={number}
-                    type="tel"
-                    name="number"
-                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    required
-                    placeholder="111-111-11-11"
-                />
-                <Button
-                    colorScheme="gray"
-                    type="submit"
-                    disabled={number && name ? false : true}
-                >
-                    {isLoading ? 'Add Contact...Spiner' : 'Add Contact'}
-                </Button>
-            </Stack>
-        </form>
+<Formik 
+initialValues={initialValues}
+onSubmit={handleSubmit}
+validationSchema={schema}
+>
+    <FormContact autoComplete="off">
+        <Label>
+            Name
+            <Input type="text" name="name"></Input>
+            <ErrorText component="div" name="name" />
+        </Label>
+        <Label>
+            Number
+            <Input type="tel" name="phone"></Input>
+            <ErrorText component="div" name="phone" />
+        </Label>
+        <Button type="submit">Add contact</Button>
+    </FormContact>
+</Formik>
     );
-}
+};
 
 
-export default ContactForm;
+
--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,1 +1,52 @@
-import {Section}
+import {Section} from '../components/Section/Section';
+import { Filter } from 'components/Filter/Filter';
+import { ContactsList } from 'components/ContactList/ContactList';
+import { ContactFrom } from 'components/ContactForm/ContactForm';
+import { Wrap } from '../components/App.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    selectContacts,
+    selectisLoading,
+    selectError,
+    selectFilteredContacts,
+} from '../redux/contacts/selectors';
+import {fetchContacts, addContact} from '../redux/contacts/operations';
+import { useEffect } from 'react';
+
+
+export default function App() {
+    const dispatch = useDispatch();
+    const contacts = useSelector(selectContacts);
+    const isLoading = useSelector(selectisLoading);
+    const error = useSelector(selectError);
+    const filter = useSelector(selectFilteredContacts);
+
+    const newContact = name => {
+        contacts.find(
+            contact => contact.name.toLowerCase() === name.name.toLowerCase()
+            )
+            ? alert(`${name.name} is already in contacts`)
+            : dispatch(addContact(name));
+    };
+
+    const handleSubmit = (values, {resetFrom}) => {
+newContact(values);
+resetFrom();
+    };
+
+    useEffect(() => {
+        dispatch(fetchContacts());
+    }, [dispatch]);
+
+        return (
+<Wrap>
+    <ContactFrom handleSubmit={handleSubmit} />
+    <Filter />
+    <Section title={`Contacts list`}>
+        {isLoading && <p>Loading contacts...</p>}
+        {error && <p>{error}</p>}
+        {contacts.length >  && <ContactsList contacts={filter} />}
+    </Section>
+</Wrap>
+        );
+}
