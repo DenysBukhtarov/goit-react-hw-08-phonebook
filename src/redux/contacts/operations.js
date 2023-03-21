8a217d8eefa21c696cbd3dc8e9c2f09498d89828
DENYS BUKHTAROV <denis180984@gmail.com>
2023-03-21 14:02:55 +0200

    fix

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,6 +1,5 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import { number } from "prop-types";
 
 export const fetchContacts = createAsyncThunk(
     'contacts/fetchAll',
